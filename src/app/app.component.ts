c77e9b85d2ffb5b71f18346b9a066df6356a725f
thomve <thomas.veran@gmail.com>
2025-02-18 20:21:10 +0100

    material and button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import {MaterialModule} from './material.module';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +10,22 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'portfolio-website';
-
   publications: any[] = [];
   projects: any[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.loadProjects();
+  }
+
+  loadProjects() {
     this.http.get('/projects').subscribe(data => {
       this.projects = data as any[];
     });
+  }
+
+  refreshProjects() {
+    this.loadProjects();
   }
 }
