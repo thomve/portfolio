1b01742a689da6390834c445fd2fc6d9ded77bab
thomve <thomas.veran@gmail.com>
2025-02-26 21:14:35 +0100

    project cards

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatChipsModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProjectsComponent {
 
